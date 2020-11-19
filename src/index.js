c9b6c8aa1a2fe4613588346c464725335a1b24ca
E. M. Greeff <unmodster@gmail.com>
2020-11-19 03:44:23 -0700

    fixed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const users = ['tim','jim','slim']
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/users', function (req, res) {
-    res.send('users')
+    res.send(users)
   })
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
