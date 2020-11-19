0ad759f724ef47e738e09cf906d8d151112674b8
E. M. Greeff <unmodster@gmail.com>
2020-11-19 03:36:20 -0700

    Step 2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+app.get('/users', function (req, res) {
+    res.send('users')
+  })
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
